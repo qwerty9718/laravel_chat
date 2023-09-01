2690c66a5e4a37a3a263bc26f4a711fdb6dd6375
Zaripov Albert <qwerty_97_18@mail.ru>
2023-09-01 21:59:25 +0500

    test chat

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestMessenger\MessageController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -33,6 +34,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::group(['prefix' => 'messages'], function (){
+        Route::get('/',[MessageController::class,'index'])->name('message.index');
+        Route::post('/',[MessageController::class,'store'])->name('message.store');
+    });
+
 });
 
 require __DIR__.'/auth.php';
